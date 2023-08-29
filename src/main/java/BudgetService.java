587e708adcff6e8a819e72eb764af6748fd834e2
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 23:21:07 +0800

    remove useless if condition

--- src/main/java/BudgetService.java
+++ src/main/java/BudgetService.java
@@ -37,37 +37,21 @@ public class BudgetService {
                 continue;
             }
             Budget budget = findBudget.get();
-
-//        }
-//        for (int i = 0; i < yearMonthsBetween.size(); i++) {
-//            if (i == 0) {
-//                for (Budget budget : listBudgets) {
             if (budget.getYearMonthInstance().equals(startYearMonth)) {
-
                 if (end.isAfter(yearMonthsBetween.get(0).atEndOfMonth())) {
                     daysBetween = DAYS.between(start, yearMonthsBetween.get(0).atEndOfMonth());
                 } else {
                     daysBetween = DAYS.between(start, end);
                 }
-
                 rtBudget += budget.dailyAmount() * (daysBetween + 1);
-            }
-//                }
-//            } else if (i == yearMonthsBetween.size() - 1) {
-//                for (Budget budget : listBudgets) {
-            else if (budget.getYearMonthInstance().equals(endYearMonth)) {
+            } else if (budget.getYearMonthInstance().equals(endYearMonth)) {
                 daysBetween = DAYS.between(yearMonth.atDay(1), end);
                 rtBudget += budget.dailyAmount() * (daysBetween + 1);
-            }
-//                }
-//            } else {
-//                for (Budget budget : listBudgets) {
-//                    if (yearMonthsBetween.get(i).equals(budget.getYearMonthInstance())) {
-            else if (yearMonth.equals(budget.getYearMonthInstance())) {
+            } else {
+//                if (yearMonth.equals(budget.getYearMonthInstance())) {
                 daysBetween = DAYS.between(budget.getYearMonthInstance().atDay(1), budget.getYearMonthInstance().atEndOfMonth());
                 rtBudget += budget.dailyAmount() * (daysBetween + 1);
             }
-//                }
         }
 
         return rtBudget; // this seems to be a placeholder, you may want to change it to return rtBudget.
