e8be68ab50829d28352a06070e5527e9ce0e96ca
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 22:42:13 +0800

    reformat

--- src/main/java/BudgetService.java
+++ src/main/java/BudgetService.java
@@ -9,55 +9,9 @@ import java.util.List;
 
 public class BudgetService {
     private final BudgetRepo budgetRepo;
-    BudgetService(){
+
+    BudgetService() {
         budgetRepo = new BudgetRepo();
-
-    }
-
-    public double query(LocalDate start, LocalDate end) {
-        if (end.isBefore(start)) {
-            return 0;
-        }
-        List<Budget> listBudgets = budgetRepo.getAll();
-        List<YearMonth> yearMonthsBetween = getYearMonthsBetween(start, end);
-
-        long daysBetween;
-        double rtBudget = 0;
-        for (int i = 0; i < yearMonthsBetween.size(); i++) {
-            if (i == 0) {
-                for (Budget budget : listBudgets) {
-                    if (isDateInYearMonth(start, toYearMonth(budget.yearMonth))) {
-
-                        if (end.isAfter(yearMonthsBetween.get(0).atEndOfMonth())){
-                            daysBetween = ChronoUnit.DAYS.between(start, yearMonthsBetween.get(0).atEndOfMonth());
-                        }else{
-                            daysBetween = ChronoUnit.DAYS.between(start, end);
-                        }
-
-                        rtBudget += budget.amount / toYearMonth(budget.yearMonth).lengthOfMonth() * (daysBetween + 1);
-                    }
-                }
-            } else if (i == yearMonthsBetween.size() - 1) {
-                for (Budget budget : listBudgets) {
-                    if (isDateInYearMonth(end, toYearMonth(budget.yearMonth))) {
-                        daysBetween = ChronoUnit.DAYS.between(yearMonthsBetween.get(i).atDay(1), end);
-                        rtBudget += budget.amount / toYearMonth(budget.yearMonth).lengthOfMonth() * (daysBetween +1);
-                    }
-                }
-            } else{
-                for (Budget budget : listBudgets) {
-                    if (yearMonthsBetween.get(i).equals(toYearMonth(budget.yearMonth)) ) {
-                        rtBudget += budget.amount ;
-                    }
-                }
-            }
-        }
-
-        return rtBudget; // this seems to be a placeholder, you may want to change it to return rtBudget.
-    }
-
-    public BudgetRepo getBudgetRepo() {
-        return this.budgetRepo;
     }
 
     private static boolean isDateInYearMonth(LocalDate date, YearMonth yearMonth) {
@@ -83,4 +37,49 @@ public class BudgetService {
         return yearMonths;
     }
 
+    public double query(LocalDate start, LocalDate end) {
+        if (end.isBefore(start)) {
+            return 0;
+        }
+        List<Budget> listBudgets = budgetRepo.getAll();
+        List<YearMonth> yearMonthsBetween = getYearMonthsBetween(start, end);
+
+        long daysBetween;
+        double rtBudget = 0;
+        for (int i = 0; i < yearMonthsBetween.size(); i++) {
+            if (i == 0) {
+                for (Budget budget : listBudgets) {
+                    if (isDateInYearMonth(start, toYearMonth(budget.yearMonth))) {
+
+                        if (end.isAfter(yearMonthsBetween.get(0).atEndOfMonth())) {
+                            daysBetween = ChronoUnit.DAYS.between(start, yearMonthsBetween.get(0).atEndOfMonth());
+                        } else {
+                            daysBetween = ChronoUnit.DAYS.between(start, end);
+                        }
+
+                        rtBudget += budget.amount / toYearMonth(budget.yearMonth).lengthOfMonth() * (daysBetween + 1);
+                    }
+                }
+            } else if (i == yearMonthsBetween.size() - 1) {
+                for (Budget budget : listBudgets) {
+                    if (isDateInYearMonth(end, toYearMonth(budget.yearMonth))) {
+                        daysBetween = ChronoUnit.DAYS.between(yearMonthsBetween.get(i).atDay(1), end);
+                        rtBudget += budget.amount / toYearMonth(budget.yearMonth).lengthOfMonth() * (daysBetween + 1);
+                    }
+                }
+            } else {
+                for (Budget budget : listBudgets) {
+                    if (yearMonthsBetween.get(i).equals(toYearMonth(budget.yearMonth))) {
+                        rtBudget += budget.amount;
+                    }
+                }
+            }
+        }
+
+        return rtBudget; // this seems to be a placeholder, you may want to change it to return rtBudget.
+    }
+
+    public BudgetRepo getBudgetRepo() {
+        return this.budgetRepo;
+    }
 }
